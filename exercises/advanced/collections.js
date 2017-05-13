8f064feb6a61a0de24c8c15c68a64e199eca5acc
SherryH <enjoy.wind7rain@gmail.com>
2017-05-13 17:46:58 +1200

    Completed advanced collection promise

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -8,9 +8,44 @@
  *    4. Writes the new file to the file located at `writePath`
  */
 
+var Promise = require('bluebird');
+var fs = require('fs');
 
+// This function returns a Promise. Returns data in .then() through 'resolve'
+// pluckFirstLineFromFile()
+// .then((firstLine)=>{})
+var pluckFirstLineFromFile = function(filePath) {
+  return new Promise((resolve, reject)=>{
+    fs.readFile(filePath, (err, data)=>{
+      if (err) {
+        reject(err);
+      } else {
+        // return the first line from file
+        resolve(data.toString().split('\n')[0]);
+      }
+    });
+  });
+};
+
+// This function returns a promise. Doesn't return any data , no 'resolve'
+// combineFirstLineOfManyFiles()
+// .then(()=>{ console.log('done!')})
+// // console.log('done!') is printed when all promiseArr resolved
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+  // TODO
+  var promiseArr = filePaths.map((filePath)=>{
+    return pluckFirstLineFromFile(filePath);
+  });
+  return Promise.all(promiseArr)
+  .then((firstLines)=>{
+    //when all promises in promiseArr are resolved,
+    // write each line to the writePath with \n in between
+    var msg = firstLines.join('\n');
+    fs.writeFile(writePath, msg, (err)=>{
+      console.log('file saved');
+    });
+  });
+
 };
 
 // Export these functions so we can unit test them
